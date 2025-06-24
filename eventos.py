1cec842d37b9c57377214be33950a59c2f7aea0f
Hesterzinha <yasmin.silva8@estudante.ifms.edu.br>
2025-06-24 18:43:00 -0400

    funçao remover eventos add

--- eventos.py
+++ eventos.py
@@ -1,3 +1,4 @@
+from participantes import participantes
 eventos = []
 ids_eventos = set()
 
@@ -46,3 +47,17 @@ def editar_evento(codigo, novo_nome=None, nova_data=None):
         evento['data'] = nova_data
     print(f"Evento {codigo} atualizado com sucesso.")
     return evento
+
+
+def remover_evento(codigo):
+    
+    global eventos
+    evento = next((e for e in eventos if e['codigo'] == codigo), None)
+    if not evento:
+        print("Evento não encontrado.")
+        return None
+    
+    [p['eventos_inscritos'].remove(codigo) for p in participantes if codigo in p['eventos_inscritos']]
+    eventos = [e for e in eventos if e['codigo'] != codigo]
+    ids_eventos.discard(codigo)
+    print(f"Evento {codigo} removido com sucesso.")
