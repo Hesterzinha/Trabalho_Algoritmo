95bcb35f700c5a68d8c85ba796be6f8dc0ce9aca
Hesterzinha <yasmin.silva8@estudante.ifms.edu.br>
2025-06-24 17:24:54 -0400

    funçoes remover e cadastar em eventos add

--- participantes.py
+++ participantes.py
@@ -1,5 +1,7 @@
+from eventos import consultar_evento
 participantes = []
 ids_participantes = set()
+
 
 def adicionar_participante(id_participante, nome, email):
     if id_participante in ids_participantes:
@@ -22,12 +24,14 @@ def consultar_participante(id_participante):
             return participante
     return None
 
+
 def listar_participantes():
     if not participantes:
         print("Nenhum participante cadastrado.")
     else:
         for participante in participantes:
             print(f"ID: {participante['id']}, Nome: {participante['nome']}, Email: {participante['email']}, Eventos: {len(participante['eventos_inscritos'])}")
+            
             
 def editar_participante(id_participante, novo_nome=None, novo_email=None):
     participante = consultar_participante(id_participante)
@@ -38,3 +42,25 @@ def editar_participante(id_participante, novo_nome=None, novo_email=None):
     if novo_email:
         participante['email'] = novo_email
     return participante
+
+
+def remover_participante(id_participante):
+    global participantes
+    participantes = [participante for participante in participantes if participante['id'] != id_participante]
+    ids_participantes.discard(id_participante)
+    
+    
+def inscrever_evento(id_participante, codigo_evento):
+    participante = consultar_participante(id_participante)
+    evento = consultar_evento(codigo_evento)
+    
+    if not participante or not evento:
+        return False
+
+    if codigo_evento in participante['eventos_inscritos']:
+        print("Participante já está inscrito neste evento.")
+        return False
+
+    participante['eventos_inscritos'].append(codigo_evento)
+    evento['participantes'].append(id_participante)
+    return True
